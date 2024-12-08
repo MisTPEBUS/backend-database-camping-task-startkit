5ee6eaf5eec336c1f58d5a2fd1fbac56e1d8a2d1
Lobinda <lobinda123@gmail.com>
2024-12-09 00:41:18 +0800

    5

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -370,23 +370,10 @@ ORDER BY
 
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
-    SELECT 
-        COALESCE(SUM("CREDIT_PURCHASE".price_paid), 0) AS 總營收
-    FROM 
-        "CREDIT_PURCHASE"
-    WHERE 
-         a.purchase_at BETWEEN '2024-11-01 00:00:00' AND '2024-12-01 00:00:00';
-
+    
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
 
-  SELECT 
-    COUNT(DISTINCT user_id) AS 預約會員人數
-    FROM  
-        "COURSE_BOOKING"
-    WHERE 
-    created_at BETWEEN '2024-11-01 00:00:00' AND '2024-11-30 23:59:59'
-    AND status NOT IN ('課程已取消');
 	
 	
 
